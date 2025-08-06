0425af32ecee08ba74fd5760f9d382aaffb1413b
丁少华 <960423114@qq.com>
2025-08-06 16:50:18 +0800

    fix

--- apps/server/bin/prisma.ts
+++ apps/server/bin/prisma.ts
@@ -2,18 +2,23 @@ import chalk from 'chalk';
 import path from 'node:path';
 import { spawnSync } from 'node:child_process';
 
-
 const rootPath = path.resolve(process.cwd());
 // const serverPath = path.resolve(rootPath, "apps", "server");
 const serverPath = rootPath;
 
-// 生成实体模型（通常位于prisma/schema.prisma）：pnpm dlx prisma db pull (生产的时候注释掉，开发阶段如果数据库变化，需要放开)
-// console.log(chalk.green('prisma: 生成实体模型'));
-// spawnSync('pnpm dlx', ['prisma', 'db', 'pull'], {
-//   cwd: serverPath, // 关键：使用 cwd 指定工作目录
-//   stdio: 'inherit', // 继承父进程的输入输出（显示日志）
-//   shell: true, // 允许使用 shell 语法（如 &&、> 等）
-// });
+// 获取命令行参数
+const args = process.argv.slice(2); // 去掉 node 和脚本路径
+const command = args[0]; // 默认执行 generate
+
+if (!command) {
+  // 生成实体模型（通常位于prisma/schema.prisma）：pnpm dlx prisma db pull (生产的时候注释掉，开发阶段如果数据库变化，需要放开)
+  console.log(chalk.green('prisma: 生成实体模型'));
+  spawnSync('pnpm dlx', ['prisma', 'db', 'pull'], {
+    cwd: serverPath, // 关键：使用 cwd 指定工作目录
+    stdio: 'inherit', // 继承父进程的输入输出（显示日志）
+    shell: true, // 允许使用 shell 语法（如 &&、> 等）
+  });
+}
 
 // 生成客户端代码（用户表操作，通常被放置在node_modules/.prisma/client目录下）--- 给你的 ts 用：pnpm dlx prisma generate
 console.log(chalk.green('prisma: 生成客户端代码'));
